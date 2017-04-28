0f753059a660dcd5db435fed5abd2df7bd298858
kirill kurinnoy <@KIRILL>
2017-04-28 09:14:11 +0300

    добавлены подсказки и инфо блок

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -174,5 +174,40 @@ namespace WindowsFormsApplication1
             HelpNavigator navigator = HelpNavigator.TopicId;
             Help.ShowHelp (this, @"C:\Users\Кирилл\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\bin\Debug\help.chm", navigator);
         }
+
+        private void dataGridView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Таблица для отображения информации о клинтах и акциях";
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Кнопка для вывода всех клиентов";
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Кнопка для вывода всех акций";
+        }
+
+        private void dataGridView2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Таблица для отображения информации владении акциями";
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "Фото акционера";
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "Информационный блок";
+        }
+
+        private void Form1_HelpRequested(object sender, HelpEventArgs hlpevent)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Кирилл\Documents\Visual Studio 2015\Projects\WindowsFormsApplication1\WindowsFormsApplication1\bin\Debug\help.chm");
+        }
     }
 }
